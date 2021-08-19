256698b4b85276b4f43db506a8dd36f16a708eba
Siddhi <dubeyriddhi0811@gmail.com>
2021-08-19 15:50:34 +0530

    Initial commit

---
+++ Riddhi_Sparks/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Riddhi_Sparks project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Riddhi_Sparks.settings')
+
+application = get_wsgi_application()
---
+++ donation/admin.py
@@ -0,0 +1,7 @@
+from donation.models import Coffee
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(Coffee)
